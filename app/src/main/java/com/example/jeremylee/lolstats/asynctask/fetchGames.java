d2c41d987d904cf068414dc6c5165da8337843a2
Jeremy Lee <jeremy.lee91@hotmail.com>
2016-04-20 16:06:31 +1000

    Fixed up fetchGames and implemented summonerGames.

--- app/src/main/java/com/example/jeremylee/lolstats/asynctask/fetchGames.java
+++ app/src/main/java/com/example/jeremylee/lolstats/asynctask/fetchGames.java
@@ -17,10 +17,12 @@ import com.example.jeremylee.lolstats.models.summonerLogin;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 /**
  * Created by jeremylee on 20/04/16.
  */
-public class fetchGames extends AsyncTask<String, Void, summonerGames> {
+public class fetchGames extends AsyncTask<String, Void, ArrayList<summonerGames>> {
 
     private final String TAG = getClass().getSimpleName();
 
@@ -43,11 +45,12 @@ public class fetchGames extends AsyncTask<String, Void, summonerGames> {
     }
 
     @Override
-    protected summonerGames doInBackground(String... args) {
+    protected ArrayList<summonerGames> doInBackground(String... args) {
         JSONObject response = null;
         String region = args[0];
         String id = args[1];
         summonerGames summonerGamesHolder = null;
+        ArrayList<summonerGames> summonerGamesList = new ArrayList<summonerGames>();
         Log.d(TAG, "We are loggin in..");
         // Save login credentials
         //LoginCredentials.setPreference(context, context.getResources().getString(R.string.entered_username), enteredUsername);
@@ -70,9 +73,21 @@ public class fetchGames extends AsyncTask<String, Void, summonerGames> {
                     String totalDamageDealt;
                     String win;
                     String assists;
+
                     JSONArray returnData = response.getJSONArray("games");
                     for(int i = 0; i < 10; i++) {
-                        returnData[i];
+                        JSONObject data = returnData.getJSONObject(i);
+                        championId = data.get("championId").toString();
+                        JSONObject stats = data.getJSONObject("stats");
+                        goldEarned = stats.get("goldEarned").toString();
+                        numDeaths = stats.get("numDeaths").toString();
+                        minionsKilled = stats.get("minionsKilled").toString();
+                        championsKilled = stats.get("championsKilled").toString();
+                        totalDamageDealt = stats.get("totalDamageDealt").toString();
+                        win = stats.get("win").toString();
+                        assists = stats.get("assists").toString();
+                        summonerGamesHolder = new summonerGames(championId, goldEarned, numDeaths, minionsKilled, championsKilled, totalDamageDealt, win, assists);
+                        summonerGamesList.add(summonerGamesHolder);
                     }
                     //String username = returnData.getString("name");
                     //String id = returnData.getString("id");
@@ -86,18 +101,18 @@ public class fetchGames extends AsyncTask<String, Void, summonerGames> {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return summonerGamesHolder;
+        return summonerGamesList;
     }
 
     @Override
-    protected void onPostExecute(summonerGames summonerGamesHolder) {
+    protected void onPostExecute(ArrayList<summonerGames> summonerGamesList) {
         // 1, 2, 3. Http online, json retrieved and no error
         if (networkAvailable && validateLoginSuccess == true) {
             // On success, go to home
             Log.d(TAG, "We got to onPostExecute");
             Intent goToStats = new Intent(activity, StatsActivity.class);
             goToStats.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            goToStats.putExtra("summonerGamesHolder", summonerGamesHolder);
+            goToStats.putExtra("summonerGamesList", summonerGamesList);
             activity.startActivity(goToStats);
             activity.finish();  // finish Login
         } else {
@@ -106,7 +121,7 @@ public class fetchGames extends AsyncTask<String, Void, summonerGames> {
             if (!networkAvailable) {
                 // If offline, show toast
                 HttpConnection.networkUnavailableToast(context);
-            } else if (summonerGamesHolder != null ) {
+            } else if (summonerGamesList != null ) {
                 // Show error message from server
 
             }
--- app/src/main/java/com/example/jeremylee/lolstats/models/summonerGames.java
+++ app/src/main/java/com/example/jeremylee/lolstats/models/summonerGames.java
@@ -4,6 +4,55 @@ package com.example.jeremylee.lolstats.models;
  * Created by jeremylee on 20/04/16.
  */
 public class summonerGames {
+    String championId;
+    String goldEarned;
+    String numDeaths;
+    String minionsKilled;
+    String championsKilled;
+    String totalDamageDealt;
+    String win;
+    String assists;
 
+    public summonerGames(String championId, String goldEarned, String numDeaths, String minionsKilled, String championsKilled, String totalDamageDealt, String win, String assists) {
+        this.championId = championId;
+        this.goldEarned = goldEarned;
+        this.numDeaths = numDeaths;
+        this.minionsKilled = minionsKilled;
+        this.championsKilled = championsKilled;
+        this.totalDamageDealt = totalDamageDealt;
+        this.win = win;
+        this.assists = assists;
+    }
 
+    public String returnChampionId() {
+        return championId;
+    }
+
+    public String returnGoldEarned() {
+        return goldEarned;
+    }
+
+    public String returnNumDeaths() {
+        return numDeaths;
+    }
+
+    public String returnMinionsKilled() {
+        return minionsKilled;
+    }
+
+    public String returnChampionsKilled() {
+        return championsKilled;
+    }
+
+    public String returnTotalDamageDealt() {
+        return totalDamageDealt;
+    }
+
+    public String returnWin() {
+        return win;
+    }
+
+    public String returnAssists() {
+        return assists;
+    }
 }
